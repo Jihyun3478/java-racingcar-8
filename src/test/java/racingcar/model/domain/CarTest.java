7bd3aae300d53922994ee7d03fa05209d9df60af
Jihyun3478 <leejihyun3478@gmail.com>
2025-10-27 20:19:12 +0900

    test: Car 클래스 내 최대 위치 확인 테스트 케이스 추가
    Car 클래스 내 최대 위치 확인 테스트 케이스 추가
    
    - 무작위 값이 4 이상일 때 전진하는 테스트 케이스 수정 -> assertRandomNumberInRangeTest 사용
    - 무작위 값이 3 이하일 때 정지하는 테스트 케이스 수정 -> assertRandomNumberInRangeTest 사용

--- src/test/java/racingcar/model/domain/CarTest.java
+++ src/test/java/racingcar/model/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.model.domain;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -11,19 +12,43 @@ public class CarTest {
     @Test
     @DisplayName("무작위 값이 4 이상이면, 전진한다.")
     public void 무작위값_4_이상이면_전진() {
-        Car car = new Car("pobi");
-        car.move(4);
-
-        assertThat(car.getPosition()).isEqualTo(1);
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Car car = new Car("pobi");
+                    car.move(4);
+                    assertThat(car.getPosition()).isEqualTo(1);
+                },
+                4
+        );
     }
 
     @Test
     @DisplayName("무작위 값이 3 이하면, 정지한다.")
     public void 무작위값_3_이하면_정지() {
-        Car car = new Car("pobi");
-        car.move(3);
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Car car = new Car("pobi");
+                    car.move(3);
+                    assertThat(car.getPosition()).isEqualTo(0);
+                },
+                3
+        );
+    }
 
-        assertThat(car.getPosition()).isEqualTo(0);
+    @Test
+    @DisplayName("최대 위치인지 확인")
+    void 최대_위치_확인() {
+        Car car = new Car("pobi");
+        assertThat(car.isMaxPosition(0)).isTrue();
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car.move(4);
+                    assertThat(car.isMaxPosition(1)).isTrue();
+                    assertThat(car.isMaxPosition(0)).isFalse();
+                },
+                4
+        );
     }
 
     @ParameterizedTest
