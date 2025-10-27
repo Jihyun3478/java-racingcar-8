136796f0cc4cd893acab9ee59df3dfbcd92ce9eb
Jihyun3478 <leejihyun3478@gmail.com>
2025-10-27 17:06:39 +0900

    refactor: TrialCount 클래스 record로 변경
    TrialCount 클래스 record로 변경
    - 아래와 같은 record 적용 기준으로 판단함
      - 불변인가? (모든 필드가 final)
      - 행동이 없거나 최소한인가?
      - 값으로 취급되는가?
      - 데이터 중심인가?

--- src/main/java/racingcar/model/domain/TrialCount.java
+++ src/main/java/racingcar/model/domain/TrialCount.java
@@ -2,17 +2,11 @@ package racingcar.model.domain;
 
 import static racingcar.constant.ErrorMessage.TRIAL_COUNT_INVALID_RANGE;
 
-public class TrialCount {
+public record TrialCount(int trialCount) {
     private static final int MINIMUM_TRIAL_COUNT = 0;
-    private final int trialCount;
 
-    public TrialCount(int trialCount) {
+    public TrialCount {
         validateNegative(trialCount);
-        this.trialCount = trialCount;
-    }
-
-    public int getTrialCount() {
-        return trialCount;
     }
 
     private void validateNegative(int trialCount) {
