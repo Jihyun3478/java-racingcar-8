060ce38a4c69c0a9443da860554b460c35a4fcd6
Jihyun3478 <leejihyun3478@gmail.com>
2025-10-27 20:19:43 +0900

    test: 시도 횟수 정상 입력 테스트 케이스 추가
    시도 횟수 정상 입력 테스트 케이스 추가
    
    - 시도 횟수는 1 이상이어야 한다.

--- src/test/java/racingcar/model/domain/TrialCountTest.java
+++ src/test/java/racingcar/model/domain/TrialCountTest.java
@@ -1,12 +1,24 @@
 package racingcar.model.domain;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class TrialCountTest {
+    @Test
+    @DisplayName("시도 횟수는 1 이상이어야 한다.")
+    void 시도_횟수_1_이상() {
+        assertThatCode(() -> new TrialCount(1))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> new TrialCount(100))
+                .doesNotThrowAnyException();
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {-10, -5, -1, 0})
     @DisplayName("시도할 횟수가 0 이하일 경우 예외가 발생한다.")
